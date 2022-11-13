07059bd981dd0b9e10765374aceb56b0aee91b88
anilsanli <sanlian@mef.edu.tr>
2022-11-13 10:22:45 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,7 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud, STOPWORDS
-import emoji
 import itertools
 from collections import Counter
 import warnings
